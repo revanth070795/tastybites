8f8ec6df75262ba34e3e537b473c13a112649577
Bogala <AH18616@LC02GL586MD6M.US.AD.WELLPOINT.COM>
2024-11-21 15:35:36 +0530

    first commit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Hero = () => {
   return (
-    <div className="relative h-[500px] bg-[url('https://images.unsplash.com/photo-1514933651103-005eec06c04b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1934&q=80')] bg-cover bg-center">
+    <div className="relative h-[100vh] bg-[url('https://images.unsplash.com/photo-1514933651103-005eec06c04b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1934&q=80')] bg-cover bg-center">
       <div className="absolute inset-0 bg-black bg-opacity-50">
         <div className="container mx-auto h-full flex flex-col justify-center items-center text-white text-center">
           <h1 className="text-5xl font-bold mb-4">Welcome to Tasty Bites</h1>
